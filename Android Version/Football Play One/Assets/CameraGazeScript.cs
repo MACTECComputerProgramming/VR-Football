c46e145e14643564c06c1c1ab0398ca1e62a1a09
20Dcpenland <20dcpenland@macon.k12.mo.us>
2018-11-30 09:37:53 -0600

    android stuff

---
+++ Android Version/Football Play One/Assets/CameraGazeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGazeScript : MonoBehaviour {
+
+    public Material lookingMaterial;
+    public Material notLookingMaterial;
+
+    void Start() {
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+
+    public void lookingSwitch() {
+        GetComponent<Renderer>().material = lookingMaterial;
+    }
+
+    public void notLooking(){
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+	
+}
