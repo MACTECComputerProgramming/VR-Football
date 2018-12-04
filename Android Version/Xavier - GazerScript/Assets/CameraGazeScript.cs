45881134f87c675544901a5cc5f7c98ce5a4ad45
Grey Spencer <19grspencer@macon.k12.mo.us>
2018-12-04 08:47:12 -0600

    Xavier - Gazer

---
+++ Android Version/Xavier - GazerScript/Assets/CameraGazeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraGazeScript : MonoBehaviour {
+
+    public Material lookingMaterial;
+    public Material notLookingMaterial;
+	public bool choice;
+	public Text UI_Text;
+
+    void Start() {
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+
+    public void lookingSwitch() {
+        GetComponent<Renderer>().material = lookingMaterial;
+
+		if (choice == true) {
+			UI_Text.text = "Correct";
+		} else {
+			UI_Text.text = "Sorry";
+		}
+    }
+
+    public void notLooking(){
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+	
+}
