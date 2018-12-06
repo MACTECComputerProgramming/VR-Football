cc4880272027044a5c4a8c2859214706e3b098f8
Romulus1408 <20dcpenland@macon.k12.mo.us>
2018-12-06 08:36:46 -0600

    Thursday

---
+++ Android Version/Football Play One/Assets/scripts/MenuActivator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuActivator : MonoBehaviour {
+
+   /* public float sightlength = 100f;
+    public GameObject selectedObj;
+    public float hoverForwardDistance = 5f;
+
+
+    void FixedUpdate()
+    {
+        RaycastHit seen;
+        Ray raydirection = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(raydirection, out seen, sightlength))
+        {
+            if (seen.collider.tag == "Button")
+            {
+                if (selectedObj != null && selctedObj != seen.transform.gameObject)
+                {
+                    GameObject hitObject = seen.transform.GameObject;
+                    MoveMenuButton(hitObject);
+
+                }
+                SelectedObj = seen.transform.GameOject;
+            }
+        }
+    }
+
+    private void MoveMenuButton(GameObject hitObject)
+    {
+        vector3 newZ = hitObject.transform.position;
+        newZ.z -= hoverForwardDistance;
+        slectedObj.transform.position = newZ;
+
+        newZ += hoverForwardDistance * 2;
+        hitObject.transform.position = newZ;
+
+    }
+    */
+
+}
