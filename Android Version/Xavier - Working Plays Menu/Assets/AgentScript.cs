86ca077ba07d994ccb9149f10a94b7e86e2e227a
Grey Spencer <19grspencer@macon.k12.mo.us>
2018-12-11 10:16:11 -0600

    Xavier - WorkingPlaysMenu
    Got the buttons to work and change scenes

---
+++ Android Version/Xavier - Working Plays Menu/Assets/AgentScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Diagnostics;
+
+public class AgentScript : MonoBehaviour {
+
+
+    
+    //PRESS SPACE TO MOVE THE PLAYERS
+
+    public Vector3 targetPositionOne;
+    public Vector3 targetPositionTwo;
+    public NavMeshAgent agent;
+//    bool second = false;
+
+   
+    Stopwatch sw = new Stopwatch();
+
+	// Use this for initialization
+	void Start ()
+    {
+        sw.Start();
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    // Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            agent.SetDestination(targetPositionOne);
+        }
+
+        if (agent.transform.position == targetPositionOne)
+//            second = true;
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            agent.SetDestination(targetPositionTwo);
+
+
+      
+
+       
+        
+
+	}
+
+
+    public void firstPostition()
+    {
+        agent.SetDestination(targetPositionOne);
+    }
+
+
+    public void secondPosition()
+    {
+        agent.SetDestination(targetPositionTwo);
+        
+    }
+}
---
+++ Android Version/Xavier - Working Plays Menu/Assets/BTN_GazerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BTN_GazerScript : MonoBehaviour {
+
+	public Button button;
+	float time;
+	float timeStop = 4.0f;
+	bool looking = false;
+
+
+
+	void Start(){
+		button = GetComponent<Button> ();
+
+	}
+
+	void Update(){
+		//constantly checks to see if your looking
+		if (looking == true) {
+			time += Time.deltaTime;
+			//Debug.Log (time);
+			if (time >= timeStop) {
+				Debug.Log ("Button clicked");
+				time = 0f;
+				ExecuteEvents.Execute (gameObject, new GvrPointerEventData (EventSystem.current), ExecuteEvents.pointerDownHandler);
+			}
+		}
+
+	}
+
+	//only fires when you are looking at an object 
+	public void Looking(){
+		looking = true;
+		Debug.Log ("Looking");
+
+	}
+
+	//cancels the looking scripts
+	public void NonLooking(){
+		looking = false;
+		Debug.Log ("Exit");
+	}
+
+	public void changeScene(int scene){
+		Debug.Log ("Pointer down");
+		SceneManager.LoadScene (scene);
+	}
+}
