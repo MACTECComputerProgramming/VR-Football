bddedf389ec6a01922a0d6afa45759e5fec273fa
Romulus1408 <20dcpenland@macon.k12.mo.us>
2018-12-14 08:30:12 -0600

    Friday-12/14/18

---
+++ Android Version/Football Play One/Assets/scripts/CameraGazeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGazeScript : MonoBehaviour {
+
+    
+    public Material lookingMaterial;
+    public Material notLookingMaterial;
+
+    public GameObject MOVE;
+
+    void Start() {
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+
+    public void lookingSwitch() {
+        GetComponent<Renderer>().material = lookingMaterial;
+    }
+
+    public void notLooking(){
+        GetComponent<Renderer>().material = notLookingMaterial;
+    }
+    
+
+    /*public void QBPOV()
+    {
+        //set Camera&*emulator to QBPOV (0.-5.-2)
+        MOVE.transform.position = transform.TransformPoint(0, -5, -2);
+        //set gazer distance to QB reqs(120)
+
+
+
+    }
+
+    public void MenuPOV()
+    {
+       
+    }
+
+    */
+}
